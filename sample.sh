44fd79642910bb875ae87c32563f77a0f3e15829
KarinHellevik <karinh5@uw.edu>
2024-07-30 08:40:23 -0400

    removed echo CUDA VISIBLE DEVICES

--- sample.sh
+++ sample.sh
@@ -5,13 +5,11 @@
 #$ -pe threads 8
 #$ -l gpu=1
 
-
 source ~/.bashrc
 conda activate /grid/shea/home/hellevik/miniconda3/envs/keypoint_moseq
 module load EBModules
 module load CUDA/11.1.1-GCC-10.2.0
 module load cuDNN/8.2.0.53-CUDA-11.1.1
 
-echo $CUDA_VISIBLE_DEVICES
 which python
-python kpmsmodelfitandreindex.py
+python autokappascan.py
